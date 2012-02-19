9db22330d89a67fdf472d3ea3988aeec38a9cb9f
Sameer Ansari <elucidation@gmail.com>
2012-02-19 18:20:23 -0500

    torques on joints added

--- simpleKin.m
+++ simpleKin.m
@@ -1,23 +1,42 @@
 
 getR = @(th) [ cosd(th) -sind(th); sind(th) cosd(th) ];
 
-for i = 0:90
-    theta=[i 30+i];
+M = 100; % kg weight on EE
+g = 9.81; % m/s^2
+A = 5; % Shoulder to arm distance
+B = 3; % arm to EE distance
+T1 = zeros(180,1);
+T2 = zeros(180,1);
+
+n = 200;
+theta_vals = linspace(0,360,n);
+
+for i = 1:n
+    theta=[theta_vals(i) 0];
 
     R = { getR(theta(1)) getR(theta(2)) };
 
-    A = [5 0]';
-    B = [3 0]';
-
-
     % Offsets
-    off1 = R{1}*A;
-    off2 = R{2}*R{1}*B;
+    off1 = R{1}*[A 0]';
+    off2 = R{2}*R{1}*[B 0]';
     k =  off1 + off2;
 
-
-    plot( [0 off1(1) k(1) ],[0 off1(2) k(2)] ,'bo-');
-    axis([-10 10 -10 10]);
-    pause(0.05);
+    % Torque on Joint 2
+    %T2 = M*g * B *
+    
+    % Graphics
+    %plot( [0 off1(1) k(1) ],[0 off1(2) k(2)] ,'bo-');
+    %hold on;
+    % Torque on joint 1
+    %plot([ 0 0 ], [0 1e-3*M*g*B*cosd(atan2(k(2),k(1)))], 'r-');
+    T1(i) = 1e-3 * M*g*B*(off1(1)+off2(1));
+    T2(i) = 1e-3 * M*g*B*off2(1);
+    % Torque on joint 2
+    %plot([ off1(1) off1(1)], [off1(2) off1(2) + 1e-3*M*g*B*cosd(sum(theta))], 'r-');
+    %axis([-10 10 -10 10]);
+    %pause(0.05);
+    %hold off;
+    
 end
 
+plot(theta_vals,T1,'r',theta_vals,T2,'b');
