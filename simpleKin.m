ca22f6744c00fa0cef6d4342834dd10ec54b795d
Sameer Ansari <elucidation@gmail.com>
2012-02-19 18:25:35 -0500

    abs torque graphs

--- simpleKin.m
+++ simpleKin.m
@@ -10,6 +10,7 @@ T2 = zeros(180,1);
 
 n = 200;
 theta_vals = linspace(0,360,n);
+GRAPHICS_ON = false;
 
 for i = 1:n
     theta=[theta_vals(i) 0];
@@ -24,19 +25,24 @@ for i = 1:n
     % Torque on Joint 2
     %T2 = M*g * B *
     
-    % Graphics
-    %plot( [0 off1(1) k(1) ],[0 off1(2) k(2)] ,'bo-');
-    %hold on;
-    % Torque on joint 1
-    %plot([ 0 0 ], [0 1e-3*M*g*B*cosd(atan2(k(2),k(1)))], 'r-');
     T1(i) = 1e-3 * M*g*B*(off1(1)+off2(1));
     T2(i) = 1e-3 * M*g*B*off2(1);
-    % Torque on joint 2
-    %plot([ off1(1) off1(1)], [off1(2) off1(2) + 1e-3*M*g*B*cosd(sum(theta))], 'r-');
-    %axis([-10 10 -10 10]);
-    %pause(0.05);
-    %hold off;
+    
+    % Graphics
+    if (GRAPHICS_ON) 
+        plot( [0 off1(1) k(1) ],[0 off1(2) k(2)] ,'bo-');
+        hold on;
+        % Torque on joint 1
+        plot([ 0 0 ], [0 1e-1*T1(i)], 'r-');
+        % Torque on joint 2
+        plot([ off1(1) off1(1)], [off1(2) off1(2) + 1e-1*T2(i)], 'r-');
+        axis([-10 10 -10 10]);
+        pause(0.05);
+        hold off;
+    end
     
 end
 
-plot(theta_vals,T1,'r',theta_vals,T2,'b');
+plot(theta_vals,abs(T1),'r',theta_vals,abs(T2),'b');
+axis([min(theta_vals) max(theta_vals) 0 max(max(T1),max(T2))]);
+legend('Shoulder','Arm');
